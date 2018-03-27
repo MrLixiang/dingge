1a5e6be66531a92dd2f8ff6fe18a55747681d60f
lixiangzhenshuai <584833754@qq.com>
2018-03-27 18:13:29 +0800

    初步提交

---
+++ pc/js/focus.js
@@ -0,0 +1,15 @@
+
+$(function(){
+    $("#searchBar").focus(function(){
+        var account_value=$(this).val();
+        if(account_value=="搜索···"){
+            $(this).val("");
+        }
+    });
+    $("#searchBar").blur(function(){
+        var account_value=$(this).val();
+        if(account_value==""){
+            $(this).val("搜索···");
+        }
+    });
+});
